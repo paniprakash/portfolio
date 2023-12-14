cfe2ea155079cabc8a25e84a6b72575a84850e50
prakash kumar panigrahi <133993660+paniprakash@users.noreply.github.com>
2023-12-14 13:11:25 +0530

    Add files via upload

---
+++ portfolioNew/script.js
@@ -0,0 +1,12 @@
+
+function openMenu(){
+    document.getElementById("for-470px").style.display="block";
+    }
+
+    function closeMenu(){
+        document.getElementById("for-470px").style.display="none";
+    }
+
+    function closeA(){
+        document.getElementById("for-470px").style.display="none";
+    }
